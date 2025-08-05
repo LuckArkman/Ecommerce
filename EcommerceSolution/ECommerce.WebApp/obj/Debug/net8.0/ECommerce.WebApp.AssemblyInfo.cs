9c011f252938c1a89b5455f3be78259096ee8df1
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-08-05 00:23:16 -0300

    update

--- EcommerceSolution/ECommerce.WebApp/obj/Debug/net8.0/ECommerce.WebApp.AssemblyInfo.cs
+++ EcommerceSolution/ECommerce.WebApp/obj/Debug/net8.0/ECommerce.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d74a34909a371ed84d173aa4e872388cc8d0e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffdc41e56aae5d9298f73bbe72c93a1e05a40420")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
